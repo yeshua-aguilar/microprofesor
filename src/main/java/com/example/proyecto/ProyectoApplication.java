f1c53ef4ad423529750f4bc37ccff3b599f9fc5c
yeshuaaguilar <yeshunator.12@gmail.com>
2023-06-30 10:17:35 -0500

    funcion de gateway

--- src/main/java/com/example/proyecto/ProyectoApplication.java
+++ src/main/java/com/example/proyecto/ProyectoApplication.java
@@ -2,9 +2,13 @@ package com.example.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @Import(AppConfig.class)
